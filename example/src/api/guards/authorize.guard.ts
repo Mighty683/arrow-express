f8b6e4e74d74aa4b1174a4374c407db276458dc1
Tomasz <tomasz93szarek@gmail.com>
2021-06-18 15:41:57 +0200

    add tests to example

--- example/src/api/guards/authorize.guard.ts
+++ example/src/api/guards/authorize.guard.ts
@@ -4,21 +4,23 @@ import Express from 'express';
 export type UserContext = {
   userId: number
 }
+
 /**
  * Stub authorization guard
  */
 export const AuthorizeGuard = async (req: Express.Request): Promise<UserContext> => {
   const authHeader = req.headers.authorization;
-  try {
-    // Here place your token authorization
-    const token = (authHeader as string).split(' ')[1];
+    const token = parseToken(authHeader);
+  // Here place your token authorization
     if (token === 'token')
       return {
         userId: 1
       };
     else
-      throw new Error();
-  } catch (_) {
-    throw new RequestError(401);
-  }
+      throw new RequestError(401);
 };
+
+
+function parseToken(authHeader: string) {
+  return (authHeader as string).split(' ')[1];
+}
